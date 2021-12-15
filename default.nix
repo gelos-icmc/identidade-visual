5d9b93f898ca54402e905e0d724d47f66fbbf0d6
Gabriel Fontes <eu@misterio.me>
2021-12-15 10:47:01 -0300

    cicd: adiciona tree html

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
-{ lib, writeText, stdenv, inkscape, fira, fontconfig }:
+{ lib, writeText, stdenv, inkscape, fira, fontconfig, tree }:
 let
+  # Gerar fontconfig com a Fira Sans inclusa
   fontConf = writeText "00-fira-fonts.conf" ''
     <?xml version='1.0'?>
     <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
@@ -12,16 +13,19 @@ stdenv.mkDerivation {
   name = "gelos-identidade-visual";
   src = ./src;
 
-  buildInputs = [ inkscape fontconfig fira ];
+  buildInputs = [ inkscape fontconfig fira tree ];
 
   FONTCONFIG_FILE=fontConf;
 
+  # Renderizar SVGs em PNGs
   buildPhase = ''
     inkscape --export-type=png *.svg -w 512 -h 512
+    tree -H '.' -L 1 --noreport --charset utf-8 -P "*.svg|*.png" -o index.html
   '';
 
+  # Mover imagens para output
   installPhase = ''
     mkdir -p $out
-    cp *.{svg,png} $out
+    cp *.{svg,png} index.html $out
   '';
 }
