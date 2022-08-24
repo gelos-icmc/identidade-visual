ffe4ebc5d0aaf2b9cfb0b346053bfeef2864b19b
Gabriel Fontes <eu@misterio.me>
2022-08-24 06:43:57 -0300

    atualizar flake

--- flake.nix
+++ flake.nix
@@ -2,23 +2,25 @@
   description = "Coleção de arquivos de identidade visual do GELOS/USP";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, utils }: {
-    # Exportar arquivos como overlay
-    overlay = final: prev: {
-      geos-identidade-visual = final.callPackage ./default.nix { };
+    overlays = rec {
+      default = gelos-identidade-visual;
+      gelos-identidade-visual = final: _prev: {
+        gelos-identidade-visual = final.callPackage ./default.nix { };
+      };
     };
   } //
   utils.lib.eachDefaultSystem (system:
-    # Importar overlay
-    let pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
+    let pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
     in rec {
-      # Exportar arquivos como package
-      packages.geos-identidade-visual = pkgs.geos-identidade-visual;
-      defaultPackage = packages.geos-identidade-visual;
+      packages = rec {
+        inherit (pkgs) gelos-identidade-visual;
+        default = gelos-identidade-visual;
+      };
     }
   );
 }
